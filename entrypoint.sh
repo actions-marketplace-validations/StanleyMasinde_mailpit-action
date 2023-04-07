668da51e01ff5b15edb6c8d7e5950452193ae782
Stanley Masinde <stanmasinde@gmail.com>
2023-04-08 02:17:14 +0300

    fix: the dockerfile image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d --name=mailpit --restart unless-stopped -p 8025:8025 -p 1025:1025 axllent/mailpit
+docker run -d --name=mailpit --restart unless-stopped -p 1025:1025 axllent/mailpit
